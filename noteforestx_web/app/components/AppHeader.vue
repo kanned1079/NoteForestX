9343598387de09ee41dd9182fb95be6790fbbf38
kanned1079 <kanna@kamiizumi.onmicrosoft.com>
2025-09-06 00:02:18 +0800

    update ui lang Sep 6

--- noteforestx_web/app/components/AppHeader.vue
+++ noteforestx_web/app/components/AppHeader.vue
@@ -1,39 +1,73 @@
 <script lang="ts" setup>
 import {ref} from "vue";
+import {updateSurfacePalette} from '@primeuix/themes';
+import {useI18n} from "vue-i18n";
 
-const selectedCountry = ref();
-const countries = ref([
-  {name: '🇨🇳 简体中文(中國)', code: 'cn'},
-  {name: '🇯🇵 日本語', code: 'jp'},
-  {name: '🇪🇸 Spain', code: 'es'},
-  {name: '🇺🇸 English(US)', code: 'en'}
-]);
+const {t, setLocale} = useI18n()
+import flagPlaceholder from "~/assets/imgs/flag_placeholder.png"
+
+const changeSurfaceColor = () => {
+  updateSurfacePalette({
+    50: '{zinc.50}',
+    // ...
+    950: '{zinc.950}'
+  });
+}
 
 const onClickLogoButton = () => {
   console.log("pressed")
 }
 
 const menu = ref();
-const items = ref([
+
+type LangItem = { label: string; code: string };
+const items: LangItem[] = [
   {
-    label: "🇨🇳 简体中文(中國)"
+    label: "English",
+    code: 'us',
   },
   {
-    label: "🇯🇵 日本語"
+    label: "简体中文",
+    code: 'cn',
   },
   {
-    label: "🇪🇸 Spain"
+    label: "繁體中文",
+    code: 'hk',
   },
   {
-    label: "🇺🇸 English(US)"
+    label: "日本語",
+    code: 'jp',
   },
-]);
+  {
+    label: "Spain",
+    code: 'es',
+  },
+  {
+    label: "Français",
+    code: 'fr',
+  },
+  {
+    label: "Suomi",
+    code: 'fi',
+  },
+  {
+    label: "Deutsch",
+    code: 'de',
+  },
+  {
+    label: "Nederlands",
+    code: 'nl',
+  },
+
+];
+
 
 const toggle = (event) => {
   menu.value.toggle(event);
 };
 
 function toggleDarkMode() {
+  console.log("toggleDarkMode");
   document.documentElement.classList.toggle('my-app-dark');
 }
 
@@ -47,30 +81,58 @@ function toggleDarkMode() {
           size="small"
           icon="pi pi-home"
           label="NoteForest X"
-          severity="secondary" variant="primary"
+          severity="secondary"
+          variant="text"
       >
+
+
       </Button>
+
     </div>
     <div class="mr-3">
 
 
-
-        <Button size="small"  type="button" label="Toggle" @click="toggle" aria-haspopup="true" aria-controls="overlay_tmenu" />
-        <TieredMenu ref="menu" id="overlay_tmenu" :model="items" popup />
-
-
-
-      <Button
-          class="ml-3"
-          size="small"
-          icon="pi pi-sun"
-          label="Sun"
-          severity="secondary" variant="primary"
-          @click="toggleDarkMode"
-      >
+      <Button size="small" variant="text" type="button" @click="toggle" aria-haspopup="true"
+              aria-controls="overlay_tmenu">
+        <i class="pi pi-language"></i>
+        语言切换
       </Button>
+      <TieredMenu size="small" appendTo="body" ref="menu" :model="items" popup class="mt-2 w-auto">
+        <template #item="{ item }">
+          <div class="flex flex-row items-center justify-between">
+            <div class="flex justify-start items-center flex-row space-x-1.5 pt-1.5 pb-1.5 pl-1.5 mb-1.25">
+              <span class="drop-shadow-sm" :class="`fi fi-${item.code} rounded-sm`"></span>
+              <span class="text-sm">{{ item.label }}</span>
+            </div>
+            <div>
+              <i v-if="item.code==='hk'" class="pi pi-check font-bold text-xs text-cyan-800 mr-3"></i>
+<!--              <Tag rounded severity="success" class="pi pi-check-circle text-sm" />-->
+            </div>
+          </div>
+
+        </template>
+      </TieredMenu>
+
+
+      <!--      <Button-->
+      <!--          class="ml-3"-->
+      <!--          size="small"-->
+      <!--          icon="pi pi-sun"-->
+      <!--          label="Sun"-->
+      <!--          severity="secondary" variant="primary"-->
+      <!--          @click="changeSurfaceColor"-->
+      <!--      >-->
+      <!--      </Button>-->
 
     </div>
   </div>
 
 </template>
+
+<style>
+/* 调整 TieredMenu 弹出距离按钮 */
+.p-tieredmenu.p-overlaypanel {
+  margin-top: 0.5rem; /* 控制下方间距 */
+  margin-left: 0.25rem; /* 控制横向偏移 */
+}
+</style>
--- noteforestx_web/nuxt.config.ts
+++ noteforestx_web/nuxt.config.ts
@@ -1,5 +1,67 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import Aura from '@primeuix/themes/aura';
+// import {MyPreset} from "./app/plugins/MyPresetTheme";
+import { definePreset } from '@primeuix/themes'
+
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        focusRing: {
+            width: '2px',
+            style: 'dashed',
+            color: '{primary.color}',
+            offset: '1px'
+        },
+        primary: {
+            50: '#e5eff7',
+            100: '#c6d9eb',
+            200: '#a7c2df',
+            300: '#88add3',
+            400: '#6998c7',
+            500: '#4f7da6',
+            600: '#3f657f',
+            700: '#2f4e5f',
+            800: '#1f3640',
+            900: '#0f1b20',
+            950: '#080f10'
+        },
+        colorScheme: {
+            light: {
+                primary: {
+                    color: '#4f7da6',
+                    inverseColor: '#ffffff',
+                    hoverColor: '#3f657f',
+                    activeColor: '#2f4e5f'
+                },
+                highlight: {
+                    background: '#4f7da6',
+                    focusBackground: '#3f657f',
+                    color: '#ffffff',
+                    focusColor: '#ffffff'
+                },
+                formField: {
+                    hoverBorderColor: '#4f7da6'
+                }
+            },
+            dark: {
+                primary: {
+                    color: '#4f7da6',
+                    inverseColor: '#ffffff',
+                    hoverColor: '#6998c7',
+                    activeColor: '#88add3'
+                },
+                highlight: {
+                    background: 'rgba(79,125,166,0.16)',
+                    focusBackground: 'rgba(79,125,166,0.24)',
+                    color: '#ffffff',
+                    focusColor: '#ffffff'
+                },
+                formField: {
+                    hoverBorderColor: '#4f7da6'
+                }
+            }
+        }
+    }
+})
 
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
@@ -16,23 +78,17 @@ export default defineNuxtConfig({
     primevue: {
         options: {
             theme: {
-                preset: Aura,
-                darkModeSelector: 'system',
-                cssLayer: false
+                preset: MyPreset,
+                // cssLayer: false
             }
         }
     },
     css: [
         '~/assets/css/main.css',
         '~/assets/css/theme.css',
-        'primeicons/primeicons.css'
+        'primeicons/primeicons.css',
+        'flag-icons/css/flag-icons.min.css'
     ],
-    // postcss: {
-    //     plugins: {
-    //         tailwindcss: {},
-    //         autoprefixer: {},
-    //     }
-    // },
     i18n: {
         locales: [
             {code: 'en', language: 'en-US', file: 'en.json'},
